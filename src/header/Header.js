c90fb13da7f396357e3c308c568f50008caca719
Tiago Pala <tiago_pala@outlook.com>
2022-03-04 12:02:32 -0300

    Add react router dom

--- src/header/Header.js
+++ src/header/Header.js
@@ -2,27 +2,28 @@ import './Header.css';
 import logo from '../images/pala-logo.png';
 import arrowdown from '../images/arrow-down-sign-to-navigate.png';
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 function Header() {
     return (
         <header>
             <Navbar className="header-nav" bg="white" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home"><img src={logo} className="App-logo" alt="logo" /></Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/"><img src={logo} className="App-logo" alt="logo" /></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" >
                         <Nav className="me-auto"></Nav>
                         <Nav className="Table-column">
-                            <Nav.Link href="#home" className="Table-cell-div links">
+                            <Nav.Link className="Table-cell-div links">
                                 <span className="link-text">Developers</span> &nbsp;
                             </Nav.Link>
-                            <Nav.Link href="#home" className="Table-cell-div links">
+                            <Nav.Link className="Table-cell-div links">
                                 <span className="link-text">Soluções Cloud</span> &nbsp;<img src={arrowdown} className="Arrow-down" alt="logo" />
                             </Nav.Link>
-                            <Nav.Link href="#link" className="Table-cell-div links ">
+                            <Nav.Link className="Table-cell-div links ">
                                 <span className="link-text">Empresa </span> &nbsp;<img src={arrowdown} className="Arrow-down" alt="logo" />
                             </Nav.Link>
-                            <Nav.Link className="Table-cell-div">
+                            <Nav.Link href="/contact" as={Link} to="/contact" className="Table-cell-div">
                                 <span className="link-text">
                                     <button className="Contact-buttom">Contato</button>
                                 </span>
---
+++ src/routing/contact/Contact.js
@@ -0,0 +1,13 @@
+import './Contact.css';
+
+function Contact() {
+    return (
+        <section className="contact-section">
+            <div className="contact-div">
+                <p>Teste</p>
+            </div>
+        </section>
+    );
+}
+
+export default Contact;
