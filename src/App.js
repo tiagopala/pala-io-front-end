5b906eb97e2e6710a75ef1b178e1134a718cfe86
Tiago Pala <tiago_pala@outlook.com>
2022-03-05 02:38:14 -0300

    alterações centralização imagem na coluna

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './header/Header.js'
 import Footer from './footer/Footer.js'
 import Body from './routing/body/Body.js'
-import Contact from './contact/Contact';
+import Contact from './routing/contact/Contact';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
